93256b2f8bcb27c6b296c156cc270573f2c18d8d
ritesh <chaudharyritesh947@gmail.com>
2021-11-12 11:17:36 +0530

    Commiting the api functionality for APOD

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
-const express = require('express');
-const mongo = require('mongo');
-const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const request = require('./request');
+const http = require('http');
+
+const dbName = 'nasa_apod';
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const todayDate = new Date().toISOString().slice(0, 10);
+const db = client.db(dbName);
+
+client.connect(function(err) {
+    console.log("Connected successfully to server");
+  
+    db.createCollection("astronomy_data", (err, res)=>{
+        if(err && err.codeName === 'NamespaceExists'){
+            console.log("Collection already present in the DB");
+        }
+        else if(err){
+            console.log("Error: ", err);
+        }
+    });
+});
+
+http.createServer((req, res)=>{
+   if(req.url === '/node'){
+        var query = { date: todayDate };
+        db.collection("astronomy_data").find(query).toArray(function(err, result) {
+          if (err) throw err;
+          if(result.length>0){
+
+            res.write(`<img alt="nasa-image" src=${result[0].url}> ${JSON.stringify(result[0])}`);
+          }
+          else{
+            request.callApi(function(responce){
+                res.write(responce);
+                const astroData = JSON.parse(responce);
+                db.collection("astronomy_data").insertOne(astroData, function(err, res) {
+                  if (err) throw err;
+                  console.log("1 document inserted");
+                });
+            })
+          }
+          res.end();
+        });      
+    }
+}).listen(3000)
+ 
---
+++ request.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const https = require('https');
+
+const todayDate = new Date().toISOString().slice(0, 10);
+
+const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${todayDate}`;
+
+const callNasaAPODApi = (callback) => {
+    https.get(url, (res) =>{
+        let data = '';
+        res.on('data', (chunk)=>{
+            data +=chunk;
+        });
+
+        res.on('end' , ()=>{
+            return callback(data)
+        });
+    }).on('error',(err)=>{
+        console.log("error"+err.message)
+    });
+}
+
+module.exports.callApi = callNasaAPODApi;
